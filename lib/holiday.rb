e2507294c4699421f8f89bbc89747f6ab20abc13
CJ Davis <csdj92@gmail.com>
2020-09-11 19:24:18 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-holiday_hash.each {|season, holiday| puts}
+holiday_hash.each {|season, holiday| puts season.to_s.cap}
 end
 
 def all_holidays_with_bbq(holiday_hash)
